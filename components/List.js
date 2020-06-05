24301e9fb2701474efeaf9b7d4d9e186cae0094a
Tr1ckC0 <trick.co@yahoo.com>
2020-06-04 20:45:17 -0500

    added getItems to list component

--- components/List.js
+++ components/List.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import {View, Button, StyleSheet, Text, FlatList } from "react-native";
+import React, {useState, useEffect} from "react";
+import { View, Button, StyleSheet, Text, FlatList } from "react-native";
 
+const URL = "http://localhost:3000";
 
-const List = ({route}) => {
-    const {list} = route.params
-    
-    return (
-        <View>
-            <Text>{list.name}</Text>
-        </View>
-    );
+const List = ({ route }) => {
+  const { list } = route.params;
+  const [items, setItems] = useState([]);
+
+  useEffect(getListItems, [])
+
+  function getListItems() {
+    fetch(URL + `/lists/${list.id}/items`)
+    .then(r => r.json())
+    .then(j => console.log(j))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{list.name}</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
 
 export default List;
