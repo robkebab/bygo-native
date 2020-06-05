409987740436334c9b3e0a73bb14fca45ec6c21b
Tr1ckC0 <trick.co@yahoo.com>
2020-06-05 12:02:18 -0500

    add list button puts a new list on the My List page

--- components/MyListsPage.js
+++ components/MyListsPage.js
@@ -1,49 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import MyLists from './MyLists';
-import {Button, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import MyLists from "./MyLists";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const URL = "http://localhost:3000";
 
-const MyListsPage = ({handlePress, userID, navigation}) => {
-  const [lists, setLists] = useState([])
+const MyListsPage = ({ handlePress, userID, navigation }) => {
+  const [lists, setLists] = useState([]);
 
-  useEffect(getLists, [])
+  useEffect(getLists, []);
+
+  function addList() {
+    fetch(URL + "/lists", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        list: {
+          name: "New List",
+          user_id: userID,
+        },
+      }),
+    })
+      .then((r) => r.json())
+      .then((list) => setLists([...lists, list]));
+  }
 
   function getLists() {
     fetch(URL + `/users/${userID}/lists`)
-    .then(r => r.json())
-    .then(j => setLists(j))
+      .then((r) => r.json())
+      .then((j) => setLists(j));
   }
 
-  // function addList() {
-  //   fetch(URL + '/lists', {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Accept: "application/json",
-  //     },
-  //     body: JSON.stringify({
-
-  //     })
-  //   })
-  // }
-
-    return (
-        <View style={styles.container}>
-            <Button title="Add List" />
-            <MyLists lists={lists} navigation={navigation}></MyLists>
-            <Button title="Log Out" onPress={handlePress} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Button title="Add List" onPress={addList} />
+      <MyLists lists={lists} navigation={navigation}></MyLists>
+      <Button title="Log Out" onPress={handlePress} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "flex-start",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
 
 export default MyListsPage;
