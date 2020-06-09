7fefa63db450e9a34d9f4cd16b2863fa95306f61
Tr1ckC0 <trick.co@yahoo.com>
2020-06-09 01:08:48 -0500

    added button to change item input

--- components/ItemInput.js
+++ components/ItemInput.js
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet, Button } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
-export default function ItemInput({addItem}) {
+export default function ItemInput({ addItem }) {
   const [newItem, setNewItem] = useState("");
+  const [adding, setAdding] = useState(false);
 
   return (
-    <View style={styles.addCont}>
-      <TextInput
-        style={styles.input}
-        placeholder="Add an item"
-        onChangeText={(text) => setNewItem(text)}
-        defaultValue={newItem}
-      />
-      <Button
-        title="Add"
-        onPress={() => {
-          addItem(newItem);
-          setNewItem("");
-        }}
-      ></Button>
-    </View>
+    <>
+      {adding ? (
+        <View style={styles.addCont}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add an item"
+            onChangeText={(text) => setNewItem(text)}
+            defaultValue={newItem}
+          />
+          <Button
+            title="Add"
+            onPress={() => {
+              addItem(newItem);
+              setNewItem("");
+              setAdding(prev => !prev)
+            }}
+          ></Button>
+        </View>
+      ) : (
+        <View style={styles.addCont}>
+          <TouchableOpacity style={styles.toggleAdd} onPress={() => setAdding(prev => !prev)}>
+            <Text style={styles.plus}>+</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </>
   );
 }
 
@@ -41,5 +60,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     // borderWidth: 2,
     // borderColor: "green"
+  },
+  toggleAdd: {
+    position: "relative",
+    textAlign: "center",
+    backgroundColor: "#00ccff",
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+  },
+  plus: {
+    position: "absolute",
+    bottom: .3,
+    left: 7.5,
+    fontSize: 40,
+    color: "white",
   },
 });
