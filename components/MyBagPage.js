9b7c2bfcabaea3732c19493ec7dbf9ea192ece8f
Tr1ckC0 <trick.co@yahoo.com>
2020-06-09 21:30:35 -0500

    less than desireable solution to update the backend when the Bag State changes

--- components/MyBagPage.js
+++ components/MyBagPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
 import ListItems from "./ListItems";
@@ -6,24 +6,52 @@ import ListItems from "./ListItems";
 import { itemsState } from "../service/itemsState";
 import { useRecoilState } from "recoil";
 
-import {replaceItemAtIndex} from "../service/helpers"
+import {URL, replaceItemAtIndex} from "../service/helpers"
+
 
 const MyBagPage = () => {
   const [items, setItems] = useRecoilState(itemsState);
 
   function removeOneItem(item) {
+    toggleClicked()
     let editedItem = {...item, checked: false}
     let idx = items.findIndex(i => i.id === item.id)
     setItems(prev => replaceItemAtIndex(prev, idx, editedItem))
   }
 
   function emptyBag() {
+    toggleClicked()
     setItems((prev) => prev.map((item) => ({ ...item, checked: false })));
   }
 
   function isEmpty() {
     return items.filter((i) => i.checked).length === 0;
   }
+
+  const [isClicked, setIsClicked] = useState(false)
+
+  function toggleClicked () {
+    setIsClicked(prev => !prev)
+  }
+
+  function updateItems() {
+    items.forEach(i => 
+      fetch(URL + `/items/${i.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          item: {
+            checked: i.checked
+          }
+        }),
+      })
+    )
+  }
+
+  useEffect(updateItems, [isClicked])
 
   return (
     <>
